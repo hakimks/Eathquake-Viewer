812cefa476a8da3d08f432f704ea9313ee45a39d
hakimks <hakimks@gmail.com>
2020-02-05 18:50:26 +0300

    feat: added a new list item layout with new fields and updated the onViewBindHolder overide methos of the adapter

--- app/src/main/java/com/kawesi/earthquake/EarthquakeRecyclerViewAdapter.java
+++ app/src/main/java/com/kawesi/earthquake/EarthquakeRecyclerViewAdapter.java
@@ -8,10 +8,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<EarthquakeRecyclerViewAdapter.ViewHolder> {
     private final List<Earthquake> mEarthquakes;
+    private static final SimpleDateFormat TIME_FORMART = new SimpleDateFormat("HH:mm", Locale.US);
+    private static final NumberFormat MAGNITUTE_FORMAT = new DecimalFormat("0.0");
 
     public EarthquakeRecyclerViewAdapter(List<Earthquake> earthquakes){
         mEarthquakes = earthquakes;
@@ -27,8 +34,10 @@ public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<Earthqua
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.earthquake = mEarthquakes.get(position);
-        holder.detailsView.setText(mEarthquakes.get(position).toString());
+        Earthquake earthquake = mEarthquakes.get(position);
+        holder.date.setText(TIME_FORMART.format(earthquake.getDate()));
+        holder.magnitute.setText(MAGNITUTE_FORMAT.format(earthquake.getMagnitute()));
+        holder.details.setText(earthquake.getMdetails());
     }
 
     @Override
@@ -37,19 +46,17 @@ public class EarthquakeRecyclerViewAdapter extends RecyclerView.Adapter<Earthqua
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        public final View parentView;
-        public final TextView detailsView;
-        public Earthquake earthquake;
+        public final TextView details;
+        public final TextView date;
+        public final TextView magnitute;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            parentView = itemView;
-            detailsView = (TextView) itemView.findViewById(R.id.list_item_earthquake_details);
+            details = (TextView) itemView.findViewById(R.id.details);
+            date = (TextView) itemView.findViewById(R.id.date);
+            magnitute = (TextView) itemView.findViewById(R.id.magnitude);
         }
 
-        @Override
-        public String toString(){
-            return super.toString() + " ' " + detailsView.getText() + " '";
-        }
     }
 }
